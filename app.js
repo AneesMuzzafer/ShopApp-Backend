2d64dcff1dda4dc5416fbbed4bf4f9aa6d12db1f
GhulamQadirKotay <ghulamqadirkotay@gmail.com>
2022-02-04 10:36:27 +0000

    From gitpod

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.use(bodyParser.json({ type: "application/json" }));
 
 mongoose.connect("mongodb+srv://admin:12345@shop-app.ylpul.mongodb.net/ShopApp?retryWrites=true&w=majority", { useNewUrlParser: true });
 
-
+app.get("/", (req, res) => {
+    res.send("Bismillah! It works");
+})
 
 app.use(products);
 app.use(orders);
@@ -24,6 +26,6 @@ app.use(images);
 app.use(users);
 
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server started at port 3000");
+app.listen(process.env.PORT || 4000, () => {
+    console.log("Server started at port 4000");
 });
